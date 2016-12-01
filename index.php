80c23e78274a7b07c607dc8b91d8b021f24b8aae
thomas1903 <tomandrewmills@gmail.com>
2016-12-01 11:35:42 +0000

    Updating index.html

--- index.php
+++ index.php
@@ -400,25 +400,25 @@
 				<div class="col-sm-6 col-md-3">
 					<div class="fun-fact text-center">
 						<h3><i class="fa fa-thumbs-o-up"></i> <span class="st-counter">365</span></h3>
-						<p>Happy Clients</p>
+						<p>Clubs</p>
 					</div>
 				</div>
 				<div class="col-sm-6 col-md-3">
 					<div class="fun-fact text-center">
 						<h3><i class="fa fa-briefcase fa-6"></i> <span class="st-counter">73987</span></h3>
-						<p>Completed Projects</p>
+						<p>Members</p>
 					</div>
 				</div>
 				<div class="col-sm-6 col-md-3">
 					<div class="fun-fact text-center">
 						<h3><i class="fa fa-coffee"></i> <span class="st-counter">297345</span></h3>
-						<p>Cups of Coffee</p>
+						<p>Club Genres</p>
 					</div>
 				</div>
 				<div class="col-sm-6 col-md-3">
 					<div class="fun-fact text-center">
 						<h3><i class="fa fa-code"></i> <span class="st-counter">9823686</span></h3>
-						<p>Lines of Code</p>
+						<p>Locations of Interest</p>
 					</div>
 				</div>
 			</div>
