be25a9f98d86817ca7840b1351af9395cd061f65
Thomas <tomandrewmills@gmail.com>
2016-12-02 22:37:46 +0000

    Made changes to the create user page.

--- createUser.php
+++ createUser.php
@@ -119,8 +119,8 @@
                         <?
                         } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
                             // execute if requested using HTTP POST Method
-                            $forename = $_POST["forename"];
-                            $surname = $_POST["surname"];
+                            $email = $_POST["email"];
+                            $password = $_POST["password"];
                             print("<h1>Hello {$forename} {$surname}</h1>");
                         }
                         ?>
