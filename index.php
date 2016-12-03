991bf31c621b9eb9f950c3bd819807f2e6ae7911
Thomas <tomandrewmills@gmail.com>
2016-12-03 18:47:07 +0000

    Added adminer.php to set up the database on mysql server.

--- index.php
+++ index.php
@@ -103,7 +103,7 @@
                         ini_set('display_startup_errors', 1);
                         error_reporting(E_ALL);
 
-                        session_start();
+
 
 
                         if(isset($_POST["signin"])) {
