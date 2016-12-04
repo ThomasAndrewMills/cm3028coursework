740fb79bac2ef0c871e0c956855188b500a546f7
Thomas <tomandrewmills@gmail.com>
2016-12-04 21:45:56 +0000

    Making changes to index.php regarding signin/logout system.

--- index.php
+++ index.php
@@ -104,10 +104,11 @@
 
 
                     <?php
+                    session_start();
                     ini_set('display_errors', 1);
                     ini_set('display_startup_errors', 1);
                     error_reporting(E_ALL);
-                    session_start();
+
                     if (isset($_POST["signin"])) {
 
                         $_SESSION['emailAddress'] = $_POST["email"];
