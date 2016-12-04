964cdf6586655de421a6a4085cd7ed0b182157e9
Thomas <tomandrewmills@gmail.com>
2016-12-04 19:55:03 +0000

    Added adminer.php to set up the database on mysql server.

--- index.php
+++ index.php
@@ -207,7 +207,7 @@
                                     <input style="font-weight: 600;border-radius: 5px;background-color: lightgray;" type="submit" value="SIGN IN/SIGN UP">
                             </form>
                             <?php
-                                if($_SESSION['loginStatus'] === TRUE){
+                                if(isset($_SESSION['loginStatus'])&& $_SESSION['loginStatus'] === TRUE){
                             ?>
                                     <form action="index.php" method="post" style="margin-top:10px;">
                                         <input style="font-weight: 600;border-radius: 5px;background-color: lightgray;" type="submit" value="LOGOUT" name="logout">
