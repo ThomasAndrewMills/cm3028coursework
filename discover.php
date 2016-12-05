73ac46f0e60714e7bdcba9a412595e5c5a6f26c9
AllyMac <alistair.macrae@sky.com>
2016-12-05 16:02:31 +0000

    embedding map

--- discover.php
+++ discover.php
@@ -2,9 +2,37 @@
 <html lang="en">
 <head>
 
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="UTF-8">
 
     <title>Go Portlethen</title>
+
+    <style>
+        /* Always set the map height explicitly to define the size of the div
+         * element that contains the map. */
+        #map {
+            height: 100%;
+        }
+        /* Optional: Makes the sample page fill the window. */
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+        #floating-panel {
+            position: absolute;
+            top: 10px;
+            left: 25%;
+            z-index: 5;
+            background-color: #fff;
+            padding: 5px;
+            border: 1px solid #999;
+            text-align: center;
+            font-family: 'Roboto','sans-serif';
+            line-height: 30px;
+            padding-left: 10px;
+        }
+    </style>
 
     <!-- Main CSS file -->
     <link rel="stylesheet" href="cluster/css/bootstrap.min.css" />
@@ -228,114 +256,89 @@
 <!-- /HEADER -->
 
 
+<div id="floating-panel">
+    <b>Start: </b>
+    <select id="start">
+        <option value="chicago, il">Chicago</option>
+        <option value="st louis, mo">St Louis</option>
+        <option value="joplin, mo">Joplin, MO</option>
+        <option value="oklahoma city, ok">Oklahoma City</option>
+        <option value="amarillo, tx">Amarillo</option>
+        <option value="gallup, nm">Gallup, NM</option>
+        <option value="flagstaff, az">Flagstaff, AZ</option>
+        <option value="winona, az">Winona</option>
+        <option value="kingman, az">Kingman</option>
+        <option value="barstow, ca">Barstow</option>
+        <option value="san bernardino, ca">San Bernardino</option>
+        <option value="los angeles, ca">Los Angeles</option>
+    </select>
+    <b>End: </b>
+    <select id="end">
+        <option value="chicago, il">Chicago</option>
+        <option value="st louis, mo">St Louis</option>
+        <option value="joplin, mo">Joplin, MO</option>
+        <option value="oklahoma city, ok">Oklahoma City</option>
+        <option value="amarillo, tx">Amarillo</option>
+        <option value="gallup, nm">Gallup, NM</option>
+        <option value="flagstaff, az">Flagstaff, AZ</option>
+        <option value="winona, az">Winona</option>
+        <option value="kingman, az">Kingman</option>
+        <option value="barstow, ca">Barstow</option>
+        <option value="san bernardino, ca">San Bernardino</option>
+        <option value="los angeles, ca">Los Angeles</option>
+    </select>
+</div>
+<div id="map"></div>
+<script>
+    function initMap() {
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 7,
+            center: {lat: 41.85, lng: -87.65}
+        });
+        directionsDisplay.setMap(map);
 
+        var onChangeHandler = function() {
+            calculateAndDisplayRoute(directionsService, directionsDisplay);
+        };
+        document.getElementById('start').addEventListener('change', onChangeHandler);
+        document.getElementById('end').addEventListener('change', onChangeHandler);
+    }
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        directionsService.route({
+            origin: document.getElementById('start').value,
+            destination: document.getElementById('end').value,
+            travelMode: 'DRIVING'
+        }, function(response, status) {
+            if (status === 'OK') {
+                directionsDisplay.setDirections(response);
+            } else {
+                window.alert('Directions request failed due to ' + status);
+            }
+        });
+    }
+</script>
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAfZtkEmnH2Ro-LnCIbwAloI05vWO53jjw&callback=initMap">
+</script>
 
 
 <!-- map -->
 <section id="map">
     <div class="container">
         <div class="row">
-            <div class="map_head">
-                <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-                <meta charset="utf-8">
-                <title>Directions service</title>
-                <style>
-                    /* Always set the map height explicitly to define the size of the div
-                     * element that contains the map. */
-                    #map {
-                        height: 100%;
-                    }
-                    /* Optional: Makes the sample page fill the window. */
-                    html, body {
-                        height: 100%;
-                        margin: 0;
-                        padding: 0;
-                    }
-                    #floating-panel {
-                        position: absolute;
-                        top: 10px;
-                        left: 25%;
-                        z-index: 5;
-                        background-color: #fff;
-                        padding: 5px;
-                        border: 1px solid #999;
-                        text-align: center;
-                        font-family: 'Roboto','sans-serif';
-                        line-height: 30px;
-                        padding-left: 10px;
-                    }
-                </style>
-            </div>
-
-            <div class="map_body"">
-            <div id="floating-panel">
-                <b>Start: </b>
-                <select id="start">
-                    <option value="chicago, il">Chicago</option>
-                    <option value="st louis, mo">St Louis</option>
-                    <option value="joplin, mo">Joplin, MO</option>
-                    <option value="oklahoma city, ok">Oklahoma City</option>
-                    <option value="amarillo, tx">Amarillo</option>
-                    <option value="gallup, nm">Gallup, NM</option>
-                    <option value="flagstaff, az">Flagstaff, AZ</option>
-                    <option value="winona, az">Winona</option>
-                    <option value="kingman, az">Kingman</option>
-                    <option value="barstow, ca">Barstow</option>
-                    <option value="san bernardino, ca">San Bernardino</option>
-                    <option value="los angeles, ca">Los Angeles</option>
-                </select>
-                <b>End: </b>
-                <select id="end">
-                    <option value="chicago, il">Chicago</option>
-                    <option value="st louis, mo">St Louis</option>
-                    <option value="joplin, mo">Joplin, MO</option>
-                    <option value="oklahoma city, ok">Oklahoma City</option>
-                    <option value="amarillo, tx">Amarillo</option>
-                    <option value="gallup, nm">Gallup, NM</option>
-                    <option value="flagstaff, az">Flagstaff, AZ</option>
-                    <option value="winona, az">Winona</option>
-                    <option value="kingman, az">Kingman</option>
-                    <option value="barstow, ca">Barstow</option>
-                    <option value="san bernardino, ca">San Bernardino</option>
-                    <option value="los angeles, ca">Los Angeles</option>
-                </select>
-            </div>
-            <div id="map"></div>
-            <script>
-                function initMap() {
-                    var directionsService = new google.maps.DirectionsService;
-                    var directionsDisplay = new google.maps.DirectionsRenderer;
-                    var map = new google.maps.Map(document.getElementById('map'), {
-                        zoom: 7,
-                        center: {lat: 41.85, lng: -87.65}
-                    });
-                    directionsDisplay.setMap(map);
-
-                    var onChangeHandler = function() {
-                        calculateAndDisplayRoute(directionsService, directionsDisplay);
-                    };
-                    document.getElementById('start').addEventListener('change', onChangeHandler);
-                    document.getElementById('end').addEventListener('change', onChangeHandler);
-                }
-
-                function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-                    directionsService.route({
-                        origin: document.getElementById('start').value,
-                        destination: document.getElementById('end').value,
-                        travelMode: 'DRIVING'
-                    }, function(response, status) {
-                        if (status === 'OK') {
-                            directionsDisplay.setDirections(response);
-                        } else {
-                            window.alert('Directions request failed due to ' + status);
-                        }
-                    });
-                }
-            </script>
-            <script async defer
-                    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAfZtkEmnH2Ro-LnCIbwAloI05vWO53jjw&callback=initMap">
-            </script>
-            </div>
+            <iframe
+                width="450"
+                height="250"
+                frameborder="0" style="border:0"
+                src="https://www.google.com/maps/embed/v1/directions
+                  ?key=AIzaSyAfZtkEmnH2Ro-LnCIbwAloI05vWO53jjw
+                  &origin=Oslo+Norway
+                  &destination=Telemark+Norway
+                  &avoid=tolls|highways" allowfullscreen>
+            </iframe>
         </div>
     </div>
 </section>
