0aaa1a1a26a5a3a64023008f2ffa95165ded3a09
AllyMac <alistair.macrae@sky.com>
2016-12-05 16:16:49 +0000

    embedding map

--- discover.php
+++ discover.php
@@ -2,11 +2,9 @@
 <html lang="en">
 <head>
 
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="UTF-8">
 
     <title>Go Portlethen</title>
-
 
     <!-- Main CSS file -->
     <link rel="stylesheet" href="cluster/css/bootstrap.min.css" />
@@ -15,33 +13,6 @@
     <link rel="stylesheet" href="cluster/css/font-awesome.css" />
     <link rel="stylesheet" href="cluster/css/style.css" />
     <link rel="stylesheet" href="cluster/css/responsive.css" />
-
-    <style>
-        /* Always set the map height explicitly to define the size of the div
-         * element that contains the map. */
-        #map {
-            height: 50%;
-        }
-        /* Optional: Makes the sample page fill the window. */
-        html, body {
-            height: 50%;
-            margin: 0;
-            padding: 0;
-        }
-        #floating-panel {
-            position: absolute;
-            top: 10px;
-            left: 25%;
-            z-index: 5;
-            background-color: #fff;
-            padding: 5px;
-            border: 1px solid #999;
-            text-align: center;
-            font-family: 'Roboto','sans-serif';
-            line-height: 30px;
-            padding-left: 10px;
-        }
-    </style>
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="cluster/images/icon/favicon.png">
@@ -119,10 +90,10 @@
             <br>
             <div class="collapse navbar-collapse" id="st-navbar-collapse">
                 <ul class="nav navbar-nav navbar-left">
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="index.php #about">About</a></li>
-                    <li><a href="club.php">Clubs</a></li>
-                    <li><a href="index.php">Team</a></li>
+                    <li><a href="#header">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#clubs">Clubs</a></li>
+                    <li><a href="#our-team">Team</a></li>
                     <li><a href="discover.php">Discover</a></li>
                     <li><a href="healthNews.php">healthy living blog</a></li>
                 </ul>
@@ -256,75 +227,9 @@
 </header>
 <!-- /HEADER -->
 
-<section id="map">
-    <div id="floating-panel">
-        <b>Start: </b>
-        <select id="start">
-            <option value="chicago, il">Chicago</option>
-            <option value="st louis, mo">St Louis</option>
-            <option value="joplin, mo">Joplin, MO</option>
-            <option value="oklahoma city, ok">Oklahoma City</option>
-            <option value="amarillo, tx">Amarillo</option>
-            <option value="gallup, nm">Gallup, NM</option>
-            <option value="flagstaff, az">Flagstaff, AZ</option>
-            <option value="winona, az">Winona</option>
-            <option value="kingman, az">Kingman</option>
-            <option value="barstow, ca">Barstow</option>
-            <option value="san bernardino, ca">San Bernardino</option>
-            <option value="los angeles, ca">Los Angeles</option>
-        </select>
-        <b>End: </b>
-        <select id="end">
-            <option value="chicago, il">Chicago</option>
-            <option value="st louis, mo">St Louis</option>
-            <option value="joplin, mo">Joplin, MO</option>
-            <option value="oklahoma city, ok">Oklahoma City</option>
-            <option value="amarillo, tx">Amarillo</option>
-            <option value="gallup, nm">Gallup, NM</option>
-            <option value="flagstaff, az">Flagstaff, AZ</option>
-            <option value="winona, az">Winona</option>
-            <option value="kingman, az">Kingman</option>
-            <option value="barstow, ca">Barstow</option>
-            <option value="san bernardino, ca">San Bernardino</option>
-            <option value="los angeles, ca">Los Angeles</option>
-        </select>
-    </div>
-    <div id="map"></div>
-    <script>
-        function initMap() {
-            var directionsService = new google.maps.DirectionsService;
-            var directionsDisplay = new google.maps.DirectionsRenderer;
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 7,
-                center: {lat: 41.85, lng: -87.65}
-            });
-            directionsDisplay.setMap(map);
 
-            var onChangeHandler = function() {
-                calculateAndDisplayRoute(directionsService, directionsDisplay);
-            };
-            document.getElementById('start').addEventListener('change', onChangeHandler);
-            document.getElementById('end').addEventListener('change', onChangeHandler);
-        }
 
-        function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-            directionsService.route({
-                origin: document.getElementById('start').value,
-                destination: document.getElementById('end').value,
-                travelMode: 'DRIVING'
-            }, function(response, status) {
-                if (status === 'OK') {
-                    directionsDisplay.setDirections(response);
-                } else {
-                    window.alert('Directions request failed due to ' + status);
-                }
-            });
-        }
-    </script>
-    <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAfZtkEmnH2Ro-LnCIbwAloI05vWO53jjw&callback=initMap">
-    </script>
-</section>
+
 
 <!-- map -->
 <section id="map">
