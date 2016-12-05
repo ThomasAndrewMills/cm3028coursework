26d847ded74b89eb102093a03ea7509b40e4ece2
AllyMac <alistair.macrae@sky.com>
2016-12-05 15:23:52 +0000

    embedding map

--- discover.php
+++ discover.php
@@ -239,8 +239,11 @@
                 width="600"
                 height="450"
                 frameborder="0" style="border:0"
-                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAfZtkEmnH2Ro-LnCIbwAloI05vWO53jjw
-                    &q=Space+Needle,Seattle+WA" allowfullscreen>
+                src="https://www.google.com/maps/embed/v1/directions
+                  ?key=AIzaSyAfZtkEmnH2Ro-LnCIbwAloI05vWO53jjw
+                  &origin=Oslo+Norway
+                  &destination=Telemark+Norway
+                  &avoid=tolls|highways" allowfullscreen>
             </iframe>
         </div>
     </div>
