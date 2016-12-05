5ab4801f40854e12455ab5127db163b11c43a9c3
Thomas <tomandrewmills@gmail.com>
2016-12-05 21:18:46 +0000

    added links to clubs

--- index.php
+++ index.php
@@ -393,89 +393,75 @@
             </div>
 
             <div class="portfolio-wrapper">
-                <div class="col-md-12">
-                    <ul class="filter">
-                        <li><a class="active" href="#" data-filter="*">All</a></li>
-                        <li><a href="#" data-filter=".wordpress">WordPress</a></li>
-                        <li><a href="#" data-filter=".html">HTML</a></li>
-                        <li><a href="#" data-filter=".graphic">graphic</a></li>
-                        <li><a href="#" data-filter=".php">PHP</a></li>
-                        <li><a href="#" data-filter=".bootstrap">bootstrap</a></li>
-                    </ul><!--/#portfolio-filter-->
-                </div>
+                <?php
+                ini_set('display_errors', 1);
+                ini_set('display_startup_errors', 1);
+                error_reporting(E_ALL);
 
-                <div class="portfolio-items">
 
-                    <div class="col-md-4 col-sm-6 work-grid wordpress graphic">
-                        <a href="discover.php">k
-                            <div class="portfolio-content">
-                                <img class="img-responsive" src="cluster/images/works/tennis.jpg" alt="">
+                // connect to server and select database
+                $db = new mysqli(
+                    "eu-cdbr-azure-west-a.cloudapp.net",
+                    "bd2505ec24d031",
+                    "a0a7a671",
+                    "goportlethendb"
+                );
+                // test if connection was established, and print any errors
+                if ($db->connect_errno) {
+                    die('Connectfailed[' . $db->connect_error . ']');
+                }
+                // create a SQL query as a string
+                $sql_query = "SELECT * FROM clubs";
+                // execute the SQL query
+                $result = $db->query($sql_query);
 
-                                <div class="portfolio-overlay">
+                //$rowsFound = $result->num_rows;
 
-                                </div>
-                                <h5>Tennis Club</h5>
-                                <p>Join our tennis Club!</p>
-                            </div>
-                        </a>
+                while ($row = $result->fetch_array()) {
+                    // print out fields from row of data
+                    echo("
+                    <div style=\"display: inline-block;width: 150px;height: 75px;margin: 10px;border: 3px solid #73AD21;\">
+                    
                     </div>
-
-
-                    <div class="col-md-4 col-sm-6 work-grid html php bootstrap">
-                        <div class="portfolio-content">
-                            <img class="img-responsive" src="cluster/images/works/hockey.jpg" alt="">
-                            <div class="portfolio-overlay">
-                                <a href="cluster/images/works/hockey.jpg"><i class="fa fa-camera-retro"></i></a>
-                                <a href="club.php?clubID=2"><h5>Hockey Club</h5></a>
-                                <p>Join our Hockey Club!</p>
-                            </div>
-                        </div>
+                    
+                    <div style=\"display: inline-block;width: 150px;height: 75px;margin: 10px;border: 3px solid #73AD21;\">
+                    
                     </div>
-
-                    <div class="col-md-4 col-sm-6 work-grid wordpress graphic">
-                        <div class="portfolio-content">
-                            <img class="img-responsive" src="cluster/images/works/football.jpg" alt="">
-                            <div class="portfolio-overlay">
-                                <a href="cluster/images/works/football.jpg"><i class="fa fa-camera-retro"></i></a>
-                                <a href="club.php?clubID=3"><h5>Football Club</h5></a>
-                                <p>Join our Football Club!</p>
-                            </div>
-                        </div>
+                    
+                    <div style=\"display: inline-block;width: 150px;height: 75px;margin: 10px;border: 3px solid #73AD21;\">
+                    
                     </div>
-
-                    <div class="col-md-4 col-sm-6 work-grid html php bootstrap">
-                        <div class="portfolio-content">
-                            <img class="img-responsive" src="cluster/images/works/martialArts.jpg" alt="">
-                            <div class="portfolio-overlay">
-                                <a href="cluster/images/works/martialArts.jpg"><i class="fa fa-camera-retro"></i></a>
-                                <a href="club.php?clubID=4"><h5>Martial Arts></h5></a>
-                                <p>Join our Martial Arts Club!</p>
-                            </div>
-                        </div>
+                    
+                    <div style=\"display: inline-block;width: 150px;height: 75px;margin: 10px;border: 3px solid #73AD21;\">
+                    
                     </div>
-
-                    <div class="col-md-4 col-sm-6 work-grid wordpress graphic php">
-                        <div class="portfolio-content">
-                            <img class="img-responsive" src="cluster/images/works/dance.jpg" alt="">
-                            <div class="portfolio-overlay">
-                                <a href="cluster/images/works/dance.jpg"><i class="fa fa-camera-retro"></i></a>
-                                <a href="club.php?clubID=5"><h5>Dance Club</h5></a>
-                                <p>Join our Dance Club!</p>
-                            </div>
-                        </div>
+                    
+                    <div style=\"display: inline-block;width: 150px;height: 75px;margin: 10px;border: 3px solid #73AD21;\">
+                    
                     </div>
-
-                    <div class="col-md-4 col-sm-6 work-grid html bootstrap graphic">
-                        <div class="portfolio-content">
-                            <img class="img-responsive" src="cluster/images/works/athletics.jpg" alt="">
-                            <div class="portfolio-overlay">
-                                <a href="cluster/images/works/athletics.jpg"><i class="fa fa-camera-retro"></i></a>
-                                <a href="club.php?clubID=6"><h5>Athletics Club</h5></a>
-                                <p>Join our Athletics Club!</p>
-                            </div>
-                        </div>
+                    
+                    <div style=\"display: inline-block;width: 150px;height: 75px;margin: 10px;border: 3px solid #73AD21;\">
+                    
                     </div>
-                </div>
+                    
+                    <div style=\"display: inline-block;width: 150px;height: 75px;margin: 10px;border: 3px solid #73AD21;\">
+                    
+                    </div>
+                    
+                    <div style=\"display: inline-block;width: 150px;height: 75px;margin: 10px;border: 3px solid #73AD21;\">
+                    
+                    </div>
+                    
+                    <div style=\"display: inline-block;width: 150px;height: 75px;margin: 10px;border: 3px solid #73AD21;\">
+                    
+                    </div>
+                    
+                    <div style=\"display: inline-block;width: 150px;height: 75px;margin: 10px;border: 3px solid #73AD21;\">
+                    
+                    </div>
+");
+                }
+                ?>
             </div>
         </div>
     </div>
