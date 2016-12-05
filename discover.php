39b757f0033955928eab822aacc1ab2fd4699eab
AllyMac <alistair.macrae@sky.com>
2016-12-05 15:25:43 +0000

    embedding map

--- discover.php
+++ discover.php
@@ -240,7 +240,7 @@
                 height="450"
                 frameborder="0" style="border:0"
                 src="https://www.google.com/maps/embed/v1/directions
-                  ?key=AIzaSyAfZtkEmnH2Ro-LnCIbwAloI05vWO53jjw
+                  ?key=AIza
                   &origin=Oslo+Norway
                   &destination=Telemark+Norway
                   &avoid=tolls|highways" allowfullscreen>
