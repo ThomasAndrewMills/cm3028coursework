7f6a0d45b01defe8031fff8feeae7fa49743cf39
Thomas <tomandrewmills@gmail.com>
2016-12-05 21:32:21 +0000

    added links to clubs

--- index.php
+++ index.php
@@ -420,14 +420,12 @@
                 while ($row = $result->fetch_array()) {
                     // print out fields from row of data
                     echo("
-                    <div style=\"display: inline-block;width: 150px;height: 75px;margin: 10px;border: 3px solid #73AD21;\">
-                        $row(clubID)<br>
-                        $row(name)<br>
-                        $row(description)<br>
-                        $row(genre)<br>
-                    </div>
-                    
-");
+                    <div style=\"display: inline-block;width: 150px;height: 75px;margin: 10px;border: 3px solid #73AD21;\">"
+                        . $row['clubID'] . "<br>"
+                        . $row['name'] . "<br>"
+                        . $row['description'] . "<br>"
+                        . $row['genre'] . "<br>
+                    </div>");
                 }
                 ?>
             </div>
