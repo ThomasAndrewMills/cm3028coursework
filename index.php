2750b148b30e05692e8fc284f8ea7bb30e4a210f
thomas1903 <tomandrewmills@gmail.com>
2016-12-05 10:09:35 +0000

    Adjusting index.php header

--- index.php
+++ index.php
@@ -88,9 +88,11 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
+        <nav class="navbar st-navbar navbar-fixed-top">
         <div class="container">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#st-navbar-collapse">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#st-navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
