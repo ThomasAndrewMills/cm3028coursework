211758e5c00380a5db808d2db13d70d2e074ffef
Thomas <tomandrewmills@gmail.com>
2016-12-09 19:24:42 +0000

    adding clubs to the index page

--- club.php
+++ club.php
@@ -79,42 +79,42 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="section-title">
-                    <h1>About</h1>
+
+                    <?php
+                    ini_set('display_errors', 1);
+                    ini_set('display_startup_errors', 1);
+                    error_reporting(E_ALL);
+
+                    $clubid = $_GET["id"];
+
+
+                    // connect to server and select database
+                    $db = new mysqli(
+                        "eu-cdbr-azure-west-a.cloudapp.net",
+                        "bd2505ec24d031",
+                        "a0a7a671",
+                        "goportlethendb"
+                    );
+                    // test if connection was established, and print any errors
+                    if ($db->connect_errno) {
+                        die('Connectfailed[' . $db->connect_error . ']');
+                    }
+                    // create a SQL query as a string
+                    $sql_query = "SELECT * FROM clubs WHERE clubID = " . $clubid . "";
+                    // execute the SQL query
+                    $result = $db->query($sql_query);
+
+                    //$rowsFound = $result->num_rows;
+
+                    while ($row = $result->fetch_array()) {
+                        // print out fields from row of data
+                        echo("<h1>" . $row['name'] . "</h1>
+            ");
+                    }
+                    ?>
                     <span class="st-border"></span>
                 </div>
             </div>
-            <?php
-            ini_set('display_errors', 1);
-            ini_set('display_startup_errors', 1);
-            error_reporting(E_ALL);
-
-            $clubid = $_GET["id"];
-
-
-            // connect to server and select database
-            $db = new mysqli(
-                "eu-cdbr-azure-west-a.cloudapp.net",
-                "bd2505ec24d031",
-                "a0a7a671",
-                "goportlethendb"
-            );
-            // test if connection was established, and print any errors
-            if ($db->connect_errno) {
-                die('Connectfailed[' . $db->connect_error . ']');
-            }
-            // create a SQL query as a string
-            $sql_query = "SELECT * FROM clubs WHERE clubID = ".$clubid."";
-            // execute the SQL query
-            $result = $db->query($sql_query);
-
-            //$rowsFound = $result->num_rows;
-
-            while ($row = $result->fetch_array()) {
-                // print out fields from row of data
-                echo("<h1>". $row['name'] . "</h1>
-            ");
-            }
-            ?>
 
             <?php
 
@@ -183,7 +183,7 @@
             <div class="col-sm-6 col-sm-push-6 footer-social-icons">
                 <span>Follow us:</span>
                 <a href="https://www.facebook.com/Portlethen-Sports-Club-703745156314817/club.php"><i
-                        class="fa fa-facebook"></i></a>
+                            class="fa fa-facebook"></i></a>
                 <a href=""><i class="fa fa-twitter"></i></a>
             </div>
             <!-- /SOCIAL ICONS -->
