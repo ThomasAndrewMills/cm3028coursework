b98cde4b74448941867535df62f17c330e9a7e31
Thomas <tomandrewmills@gmail.com>
2016-12-09 21:25:16 +0000

    adding clubs to the index page

--- index.php
+++ index.php
@@ -437,6 +437,58 @@
 <!-- /OUR WORKS -->
 
 
+
+
+<!-- TESTIMONIAL -->
+<section id="testimonial">
+    <div class="container">
+        <div class="row">
+            <div class="overlay"></div>
+            <div class="col-md-8 col-md-offset-2 col-sm-12">
+                <div class="st-testimonials">
+
+                    <div class="item active text-center">
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolore nesciunt natus ullam
+                            possimus quas obcaecati suscipit voluptate facilis cum"</p>
+                        <div class="st-border"></div>
+                        <div class="client-info">
+                            <h5>Tom Roof</h5>
+                            <span>CEO of Domain.com</span>
+                        </div>
+                    </div>
+
+                    <div class="item text-center">
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolore nesciunt natus ullam
+                            possimus quas obcaecati suscipit voluptate facilis cumconsectetur adipisicing elit. Amet
+                            dolore"</p>
+                        <div class="st-border"></div>
+                        <div class="client-info">
+                            <h5>Mustafiz</h5>
+                            <span>CEO of Domain.com</span>
+                        </div>
+                    </div>
+
+                    <div class="item text-center">
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem autem possimus laborum,
+                            ducimus vel rerum asperiores delectus, suscipit voluptate mollitia, ullam perspiciatis
+                            voluptates!"</p>
+                        <div class="st-border"></div>
+                        <div class="client-info">
+                            <h5>Sean Hynes</h5>
+                            <span>CEO of Domain.com</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /TESTIMONIAL -->
+
+
+
+
 <!-- OUR TEAM -->
 <section id="our-team">
     <div class="container">
@@ -524,52 +576,7 @@
 </section>
 <!-- /OUR TEAM -->
 
-<!-- TESTIMONIAL -->
-<section id="testimonial">
-    <div class="container">
-        <div class="row">
-            <div class="overlay"></div>
-            <div class="col-md-8 col-md-offset-2 col-sm-12">
-                <div class="st-testimonials">
 
-                    <div class="item active text-center">
-                        <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolore nesciunt natus ullam
-                            possimus quas obcaecati suscipit voluptate facilis cum"</p>
-                        <div class="st-border"></div>
-                        <div class="client-info">
-                            <h5>Tom Roof</h5>
-                            <span>CEO of Domain.com</span>
-                        </div>
-                    </div>
-
-                    <div class="item text-center">
-                        <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolore nesciunt natus ullam
-                            possimus quas obcaecati suscipit voluptate facilis cumconsectetur adipisicing elit. Amet
-                            dolore"</p>
-                        <div class="st-border"></div>
-                        <div class="client-info">
-                            <h5>Mustafiz</h5>
-                            <span>CEO of Domain.com</span>
-                        </div>
-                    </div>
-
-                    <div class="item text-center">
-                        <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem autem possimus laborum,
-                            ducimus vel rerum asperiores delectus, suscipit voluptate mollitia, ullam perspiciatis
-                            voluptates!"</p>
-                        <div class="st-border"></div>
-                        <div class="client-info">
-                            <h5>Sean Hynes</h5>
-                            <span>CEO of Domain.com</span>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<!-- /TESTIMONIAL -->
 
 
 <!-- FUN FACTS -->
