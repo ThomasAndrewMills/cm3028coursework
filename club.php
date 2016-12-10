b8c6748b8b935ed1225206833053c4b7a114f4a5
Thomas <tomandrewmills@gmail.com>
2016-12-10 00:24:06 +0000

    adding clubs to the index page

--- club.php
+++ club.php
@@ -156,7 +156,7 @@
                 echo("");
                 ?>
             </div>
-            <div style="display: inline-block;width:320px;background-color: #de9c5f;">
+            <div style="display: inline-block;width:380px;background-color: #de9c5f;">
                 <h4 class="sidebar-title">Post an Article</h4>
                 <ul>
                     <a href="createNewsStory.php">Click here to post a new article.</a>
