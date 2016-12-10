eb689a4ac2ea705b60dcb6f52a2a8b98d3ce741a
Thomas <tomandrewmills@gmail.com>
2016-12-10 16:20:00 +0000

    adding clubs to the index page

--- index.php
+++ index.php
@@ -287,7 +287,7 @@
                         <div class="col-sm-7">
                             <h1>Discover</h1>
                             <h2>North Kincardineshire</h2>
-                            <p style="color:white;">Discover new places and plan your routes around Portlethen</p>
+                            <p style="color:white;font-weight: normal;">Discover new places and plan your routes around Portlethen</p>
                         </div>
                     </div>
                 </div>
@@ -298,7 +298,7 @@
                         <div class="col-sm-7">
                             <h1>Sports in your area</h1>
                             <h2>stay active</h2>
-                            <p style="color:white;">Discover sports and clubs in the Portlethen Community</p>
+                            <p style="color:white;font-weight: normal;">Discover sports and clubs in the Portlethen Community</p>
                         </div>
                     </div>
                 </div>
@@ -309,7 +309,7 @@
                         <div class="col-sm-7">
                             <h1>health and wellbeing</h1>
                             <h2>be healthy</h2>
-                            <p style="color:white;">Discover the latest news and information to ensure a healthy lifestyle</p>
+                            <p style="color:white;font-weight: normal;">Discover the latest news and information to ensure a healthy lifestyle</p>
                         </div>
                     </div>
                 </div>
