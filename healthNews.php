29afbde1da1de7c19a26c4624718fc1cfd810181
Thomas <tomandrewmills@gmail.com>
2016-12-10 19:31:05 +0000

    adding clubs to the index page

--- healthNews.php
+++ healthNews.php
@@ -123,7 +123,9 @@
                     // print out fields from row of data
                     echo (
                     '<div class="single-blog"style="display: block;">
-						<article>
+						<article style="-webkit-box-shadow: 0px 0px 18px 5px rgba(0,0,0,0.75);
+-moz-box-shadow: 0px 0px 18px 5px rgba(0,0,0,0.75);
+box-shadow: 0px 0px 18px 5px rgba(0,0,0,0.75);">
 							<div class="post-thumb"><img class="img-responsive" src="cluster/images/blog/01.jpg" alt=""></div>
 							
 							<div style="float:left;display:block;width:100%;margin:5px;">
