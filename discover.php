284645367c2f0073484c0b77073fd88c5ea0f08e
Thomas <tomandrewmills@gmail.com>
2016-12-10 19:45:32 +0000

    adding clubs to the index page

--- discover.php
+++ discover.php
@@ -75,50 +75,153 @@
     <nav class="navbar st-navbar navbar-fixed-top">
 
 
-        <div style="text-align:center; font-size:30px;font-family: 'Raleway'";>
-            <a class="logo"  href="index.php"><b>GO PORTLETHEN</b></a>
-        </div>
+        <!--<div style="text-align:left; font-size:30px;font-family: 'Raleway'" ;>
+            <a class="logo" href="index.php"><b>GO PORTLETHEN</b></a>
+        </div>-->
 
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#st-navbar-collapse">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
         </button>
-        <div class="container">
-
-            <br>
-            <div class="collapse navbar-collapse" id="st-navbar-collapse">
-                <ul class="nav navbar-nav navbar-left">
-                    <li><a href="#header">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#clubs">Clubs</a></li>
-                    <li><a href="#our-team">Team</a></li>
-                    <li><a href="discover.php">Discover</a></li>
-                    <li><a href="healthNews.php">healthy living blog</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-
-
-                    <?php
-                    session_start();
-                    $_SESSION['loginStatus'] = FALSE;
-
-                    ini_set('display_errors', 1);
-                    ini_set('display_startup_errors', 1);
-                    error_reporting(E_ALL);
+        <nav class="navbar st-navbar navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                            data-target="#st-navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="logo" href="index.php"><b style="font-size: 20px;">GOPORTLETHEN</b></a>
+                </div>
+                <div class="collapse navbar-collapse" id="st-navbar-collapse">
+                    <ul class="nav navbar-nav navbar-left">
+                        <li><a href="#header">Home</a></li>
+                        <li><a href="#clubs">Clubs</a></li>
+                        <li><a href="discover.php">Discover</a></li>
+                        <li><a href="healthNews.php">healthy living blog</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
 
 
-                    if(isset($_POST["logout"])) {
-                        session_destroy();
-                    }
+                        <?php
+                        session_start();
+                        if (isset($_POST["logout"])) {
+                            session_destroy();
+                        }
+
+                        ini_set('display_errors', 1);
+                        ini_set('display_startup_errors', 1);
+                        error_reporting(E_ALL);
 
 
-                    if(isset($_POST["signin"])) {
-                        $email = $_POST["email"];
-                        $password = $_POST["password"];
+                        if (isset($_SESSION['loginStatus'])) {
+                            if ($_SESSION['loginStatus'] === TRUE) {
+                                //check the email and password
+                                // connect to server and select database
+                                $db = new mysqli(
+                                    "eu-cdbr-azure-west-a.cloudapp.net",
+                                    "bd2505ec24d031",
+                                    "a0a7a671",
+                                    "goportlethendb"
+                                );
+                                // test if connection was established, and print any errors
+                                if ($db->connect_errno) {
+                                    die('Connectfailed[' . $db->connect_error . ']');
+                                }
+                                // create a SQL query as a string
+                                $sql_query = "SELECT * FROM users WHERE emailAddress='" . $_SESSION['emailAddress'] . "' AND password='" . $_SESSION['password'] . "' LIMIT 1";
+                                // execute the SQL query
+                                $result = $db->query($sql_query);
 
-                        if (isset($email)) {
+                                $rowsFound = $result->num_rows;
+
+                                while ($row = $result->fetch_array()) {
+                                    // print out fields from row of data
+                                    $_SESSION['displayName'] = $row['displayName'];
+                                }
+
+                                if ($rowsFound === 1) {
+
+                                    $_SESSION['loginStatus'] = TRUE;
+                                }
+
+                                if ($rowsFound === 0) {
+                                    $_SESSION['loginStatus'] = FALSE;
+                                }
+
+                                $result->close();
+                                // close connection to database
+                                $db->close();
+                            }
+                        }
+
+
+                        if (isset($_POST["signin"])) {
+                            $_SESSION['emailAddress'] = $_POST["email"];
+                            $_SESSION['password'] = $_POST["password"];
+
+                            //check the email and password
+                            // connect to server and select database
+                            $db = new mysqli(
+                                "eu-cdbr-azure-west-a.cloudapp.net",
+                                "bd2505ec24d031",
+                                "a0a7a671",
+                                "goportlethendb"
+                            );
+                            // test if connection was established, and print any errors
+                            if ($db->connect_errno) {
+                                die('Connectfailed[' . $db->connect_error . ']');
+                            }
+                            // create a SQL query as a string
+                            $sql_query = "SELECT * FROM users WHERE emailAddress='" . $_SESSION['emailAddress'] . "' AND password='" . $_SESSION['password'] . "' LIMIT 1";
+                            // execute the SQL query
+                            $result = $db->query($sql_query);
+
+                            $rowsFound = $result->num_rows;
+
+                            while ($row = $result->fetch_array()) {
+                                // print out fields from row of data
+                                $_SESSION['displayName'] = $row['displayName'];
+                            }
+
+                            if ($rowsFound === 1) {
+
+                                $_SESSION['loginStatus'] = TRUE;
+                            }
+
+                            if ($rowsFound === 0) {
+                                $_SESSION['loginStatus'] = FALSE;
+                            }
+
+                            $result->close();
+                            // cl ose connection to database
+                            $db->close();
+                        }
+
+                        if (isset($_SESSION['loginStatus'])) {
+                            //if user is logged in display username
+                            if ($_SESSION['loginStatus'] === TRUE) {
+                                echo("Logged in as " . $_SESSION['displayName']);
+                                //if user login was false
+                            } elseif ($_SESSION['loginStatus'] === FALSE && isset($_POST["signin"])) {
+                                echo('<li style="margin: 14px;"><b>' . "INCORRECT USERNAME OR PASSWORD" . '</b></li>');
+                            } elseif ($_SESSION['loginStatus'] === FALSE) {
+                                echo('<li style="margin: 14px;"><b>' . "YOU ARE NOT LOGGED IN" . '</b></li>');
+                            }
+                        } else {
+                            echo('<li style="margin: 14px;"><b>' . "YOU ARE NOT LOGGED IN" . '</b></li>');
+                        }
+
+
+                        //if the signup form in submitted from createUser.php
+                        if (isset($_POST["signup"])) {
+                            $name = $_POST["name"];
+                            $password = $_POST["password"];
+                            $emailAddress = $_POST["emailAddress"];
+                            $displayName = $_POST["displayName"];
+
 
                             // connect to server and select database
                             $db = new mysqli(
@@ -132,98 +235,43 @@
                                 die('Connectfailed[' . $db->connect_error . ']');
                             }
                             // create a SQL query as a string
-                            $sql_query = "SELECT * FROM users WHERE emailAddress='$email' AND password='$password' LIMIT 1";
-                            // execute the SQL query
-                            $result = $db->query($sql_query);
-
-                            $rowsFound = $result->num_rows;
-
-                            while($row = $result->fetch_array()){
-                                // print out fields from row of data
-                                $displayName = $row['displayName'];
-                            }
-
-                            if ($rowsFound === 1) {
-                                echo("Logged in as " . $displayName);
-                                $_SESSION['loginStatus'] = TRUE;
-                            }
-
-
-                            if ($rowsFound === 0){
-                                echo('<b>' . "Incorrect email address or password" . '</b>');
-                            }
-
-                            $result->close();
-                            // cl ose connection to database
-                            $db->close();
-
-                        }
-                    }
-
-
-
-
-
-
-
-                    //-------------------------
-
-
-
-
-
-
-
-                    //-------------------------
-                    if (isset($_POST["signup"])) {
-                        $name = $_POST["name"];
-                        $password = $_POST["password"];
-                        $emailAddress = $_POST["emailAddress"];
-                        $displayName = $_POST["displayName"];
-
-
-                        // connect to server and select database
-                        $db = new mysqli(
-                            "eu-cdbr-azure-west-a.cloudapp.net",
-                            "bd2505ec24d031",
-                            "a0a7a671",
-                            "goportlethendb"
-                        );
-                        // test if connection was established, and print any errors
-                        if ($db->connect_errno) {
-                            die('Connectfailed[' . $db->connect_error . ']');
-                        }
-                        // create a SQL query as a string
-                        $sql_query = "INSERT INTO users (name, password, emailAddress,displayName)
+                            $sql_query = "INSERT INTO users (name, password, emailAddress,displayName)
                                           VALUES ('$name', '$password', '$emailAddress','$displayName')";
-                        // execute the SQL query
-                        if ($db->query($sql_query) === TRUE) {
-                            echo "Signup successful!";
-                        } else {
-                            echo "Error: " . $sql_query . "<br>" . $db->error;
-                        }
+                            // execute the SQL query
+                            if ($db->query($sql_query) === TRUE) {
+                                echo "Signup successful!";
+                            } else {
+                                echo "Error: " . $sql_query . "<br>" . $db->error;
+                            }
 
-                        $db->close();
-                    }
-                    ?>
-                    <li>
-                        <form action="createUser.php" style="margin-top:10px;">
-                            <input style="font-weight: 600;border-radius: 5px;background-color: lightgray;" type="submit" value="SIGN IN/SIGN UP">
-                        </form>
-                        <?php
-                        if($_SESSION['loginStatus'] === TRUE){
-                            ?>
-                            <form action="index.php" method="post" style="margin-top:10px;">
-                                <input style="font-weight: 600;border-radius: 5px;background-color: lightgray;" type="submit" value="LOGOUT" name="logout">
-                            </form>
-                            <?php
+                            $db->close();
                         }
                         ?>
-                    </li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container -->
-    </nav>
+                        <li>
+
+                            <?php
+                            if (isset($_SESSION['loginStatus']) && $_SESSION['loginStatus'] === TRUE) {
+                                ?>
+                                <form action="index.php" method="post" style="margin-top:10px;">
+                                    <input style="font-weight: 600;border-radius: 5px;background-color: lightgray;"
+                                           type="submit" value="LOGOUT" name="logout" class="button">
+                                </form>
+                                <?php
+                            } else {
+                                ?>
+                                <form action="createUser.php" style="margin-top:10px;" >
+                                    <input style="font-weight: 600;border-radius: 5px;background-color: lightgray;"
+                                           type="submit" value="SIGN IN/SIGN UP" class="button">
+                                </form>
+                                <?php
+                            }
+
+                            ?>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container -->
+        </nav>
 </header>
 <!-- /HEADER -->
 
