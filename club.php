afd1d064bfe9adb4d1d91f8371fe3b6197c96f7c
Thomas <tomandrewmills@gmail.com>
2016-12-11 17:43:22 +0000

    Adding login restrictions to pages.

--- club.php
+++ club.php
@@ -350,7 +350,7 @@
                                <img src="get_image.php?mediaID=' . $row['mediaID'] .'" alt="' . $row['caption'] . '">
                           </div>');
                     }
-                    $counter++;
+                    $counter = counter + 1;
                 }
 
 
