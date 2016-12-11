54529cecd7d61fde0797ad8bee22e119b76067d1
Thomas <tomandrewmills@gmail.com>
2016-12-11 01:14:57 +0000

    adding clubs to the index page

--- healthNews.php
+++ healthNews.php
@@ -294,7 +294,7 @@
                         die('Connectfailed[' . $db->connect_error . ']');
                     }
                     // create a SQL query as a string
-                    $sql_query = "DELETE FROM healthnews WHERE articalID='" . $articalID . "'";
+                    $sql_query = "DELETE FROM healthnews WHERE articleID='" . $articalID . "'";
                     // execute the SQL query
                     if ($db->query($sql_query) === TRUE) {
                         echo "Upload successful!";
