e42196f3446eabe0ca23c88c9dfed3178c0e1bad
Callum <callummelia@gmail.com>
2016-12-12 21:01:50 +0000

    Testing

--- index.php
+++ index.php
@@ -110,6 +110,7 @@
                         if (isset($_POST["logout"])) {
                             session_destroy();
                             header("Location: index.php");
+                            exit;
                         }
                         ini_set('display_errors', 1);
                         ini_set('display_startup_errors', 1);
