5864f05118afd8a1b9b2e3face3bbcafe2ff36b1
Callum <callummelia@gmail.com>
2016-12-12 12:30:56 +0000

    Trying to implement map system....

--- discover.php
+++ discover.php
@@ -295,7 +295,7 @@
                     <option value="st louis, mo">St Louiss</option>
                 </select>
                 </div>
-                <div id"map"></div>
+                <div id"maptwo"></div>
                 <script>
                 function initMap()  {
                     var directionsService = new google.maps.DirectionsService;
