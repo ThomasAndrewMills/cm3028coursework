043b8a2d686283cf81b170dea00009d880dfcc4c
thomas1903 <tomandrewmills@gmail.com>
2016-12-12 15:05:20 +0000

    Removing team section from index page

--- index.php
+++ index.php
@@ -249,36 +249,7 @@
                             $db->close();
                         }
 
-                        if (isset($_POST["createClub"])) {
-                            $clubName = $_POST["clubName"];
-                            $aboutClub = $_POST["aboutClub"];
-                            $emailAddress = $_SESSION
-                            $displayName = $_POST["displayName"];
-
-
-                            // connect to server and select database
-                            $db = new mysqli(
-                                "eu-cdbr-azure-west-a.cloudapp.net",
-                                "bd2505ec24d031",
-                                "a0a7a671",
-                                "goportlethendb"
-                            );
-                            // test if connection was established, and print any errors
-                            if ($db->connect_errno) {
-                                die('Connectfailed[' . $db->connect_error . ']');
-                            }
-                            // create a SQL query as a string
-                            $sql_query = "INSERT INTO users (name, password, emailAddress,displayName)
-                                          VALUES ('$name', '$password', '$emailAddress','$displayName')";
-                            // execute the SQL query
-                            if ($db->query($sql_query) === TRUE) {
-                                echo "Signup successful!";
-                            } else {
-                                echo "Error: " . $sql_query . "<br>" . $db->error;
-                            }
-
-                            $db->close();
-                        }
+                        
                         ?>
                         <li>
 
